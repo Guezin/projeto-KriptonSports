cabe03c362401e37e1a252856450b59376cd18b1
Guezin <leandroguezinjunior@gmail.com>
2020-08-16 12:58:45 -0300

    pagina createProduct criada

--- web/src/pages/CreateProduct/index.tsx
+++ web/src/pages/CreateProduct/index.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import {
+  RiBarcodeLine,
+  RiCoinsLine,
+  RiCalendar2Line,
+  RiStore3Line,
+} from 'react-icons/ri';
+import { TiSortNumericallyOutline } from 'react-icons/ti';
+import { Form } from '@unform/web';
+
+import Header from '../../components/Header';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container } from './styles';
 
 const CreateProduct: React.FC = () => {
+  const handleSubmit = useCallback(data => {
+    console.log(data);
+  }, []);
+
   return (
     <Container>
-      <h1>CreateProduct</h1>
+      <Header />
+
+      <fieldset>
+        <legend>Adicionar produto</legend>
+
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="name"
+            icon={RiStore3Line}
+            placeholder="Nome"
+          />
+          <Input
+            type="number"
+            name="quantity"
+            icon={TiSortNumericallyOutline}
+            placeholder="Quantidade"
+          />
+          <Input
+            type="text"
+            name="price"
+            icon={RiCoinsLine}
+            placeholder="Preço R$0,00"
+          />
+          <Input
+            type="number"
+            name="product_code"
+            icon={RiBarcodeLine}
+            placeholder="Código de barras"
+          />
+          <Input
+            type="text"
+            name="expiration_date"
+            icon={RiCalendar2Line}
+            placeholder="Validade dd/mm/aaaa"
+          />
+
+          <Button type="submit">Adicionar</Button>
+        </Form>
+      </fieldset>
     </Container>
   );
 };
--- web/src/pages/CreateProduct/styles.ts
+++ web/src/pages/CreateProduct/styles.ts
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  fieldset {
+    border: 0;
+
+    legend {
+      padding: 2rem;
+      font: 600 2.4rem 'Poppins';
+      color: var(--color-text-secondary);
+    }
+
+    form {
+      padding: 2rem;
+
+      div + div {
+        margin-top: 0.8rem;
+      }
+
+      button {
+        margin-top: 0.8rem;
+      }
+    }
+  }
+`;
