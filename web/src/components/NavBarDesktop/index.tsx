e655fed7ee4e636addec4e580ef176ea514db2ca
Guezin <leandroguezinjunior@gmail.com>
2020-08-26 18:44:53 -0300

    informando ao usuario em qual navegacao ele esta, apos selecionar menu no desktop

--- web/src/components/NavBarDesktop/index.tsx
+++ web/src/components/NavBarDesktop/index.tsx
@@ -1,28 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { RiHome3Line, RiAddLine, RiBarcodeLine } from 'react-icons/ri';
 
 import { Container } from './styles';
 
 const NavBarDesktop: React.FC = () => {
+  const [selected, setSelected] = useState(0);
+
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    switch (pathname) {
+      case '/home':
+        setSelected(1);
+        break;
+      case '/create-product':
+        setSelected(2);
+        break;
+      case '/products':
+        setSelected(3);
+        break;
+      default:
+        setSelected(selected);
+    }
+  }, [pathname, selected]);
+
   return (
-    <Container>
+    <Container selected={selected}>
       <nav>
         <ul>
-          <li>
-            <Link to="/home">Home</Link>
-            <RiHome3Line size={20} color="#fff" />
-          </li>
+          <Link to="/home" onClick={() => setSelected(1)}>
+            <li>
+              Home
+              <RiHome3Line size={20} color="#fff" />
+            </li>
+          </Link>
 
-          <li>
-            <Link to="/create-product">Cadastrar</Link>
-            <RiAddLine size={20} color="#fff" />
-          </li>
+          <Link to="/create-product" onClick={() => setSelected(2)}>
+            <li>
+              Cadastrar
+              <RiAddLine size={20} color="#fff" />
+            </li>
+          </Link>
 
-          <li>
-            <Link to="/products">Produtos</Link>
-            <RiBarcodeLine size={20} color="#fff" />
-          </li>
+          <Link to="/products" onClick={() => setSelected(3)}>
+            <li>
+              Produtos
+              <RiBarcodeLine size={20} color="#fff" />
+            </li>
+          </Link>
         </ul>
       </nav>
     </Container>
--- web/src/components/NavBarDesktop/styles.ts
+++ web/src/components/NavBarDesktop/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+interface IContainerProps {
+  selected: number;
+}
+
+export const Container = styled.div<IContainerProps>`
   width: 25.6rem;
   height: 100%;
   background-color: var(--color-background-secondary);
@@ -13,18 +17,26 @@ export const Container = styled.div`
       flex-direction: column;
       align-items: center;
 
-      li {
+      a {
         width: 100%;
         padding: 1.6rem 3.2rem;
 
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-
-        a {
+        li {
           font: 500 1.6rem 'Poppins';
           color: var(--color-text-title);
+
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
         }
+      }
+
+      a:nth-child(${props => props.selected}) {
+        border-left: 2px solid #fff;
+      }
+
+      a:hover {
+        border-left: 2px solid #fff;
       }
     }
   }
