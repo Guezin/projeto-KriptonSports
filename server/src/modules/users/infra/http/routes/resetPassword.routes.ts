d55c29eda51ff50cebaf7b4acf162eeb017f1403
Guezin <leandroguezinjunior@gmail.com>
2020-09-01 20:23:24 -0300

    validacao dos dados para resete de senha

--- server/src/modules/users/infra/http/routes/resetPassword.routes.ts
+++ server/src/modules/users/infra/http/routes/resetPassword.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { Segments, Joi, celebrate } from 'celebrate';
 
 import ResetPasswordController from '../controllers/ResetPasswordController';
 
 const routes = Router();
 const resetPassword = new ResetPasswordController();
 
-routes.post('/', resetPassword.create);
+routes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      token: Joi.string().required(),
+      password: Joi.string().required(),
+    }),
+  }),
+  resetPassword.create
+);
 
 export default routes;
