c2dd4066283e86eafdfc495b19d41a0d30c2e74f
Guezin <leandroguezinjunior@gmail.com>
2020-09-01 20:25:01 -0300

    validacao dos dados para rota forgot-password

--- server/src/modules/users/infra/http/routes/forgotPassword.routes.ts
+++ server/src/modules/users/infra/http/routes/forgotPassword.routes.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
+import { Segments, Joi, celebrate } from 'celebrate';
 
 import ForgotPasswordController from '../controllers/ForgotPasswordController';
 
 const routes = Router();
 const forgotPasswordController = new ForgotPasswordController();
 
-routes.post('/', forgotPasswordController.create);
+routes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().email().required(),
+    }),
+  }),
+  forgotPasswordController.create
+);
 
 export default routes;
